824f42c8f370607c8086c6cac03650a9ccd7f3c6
Henry AT <henry.ambicho@mercadolibre.cl>
2016-08-30 16:56:26 -0400

    Initial Commit

---
+++ PryEmptyWithMock/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PryEmptyWithMock
+//
+//  Created by Henry Ambicho Trujillo on 8/30/16.
+//  Copyright © 2016 MELI. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
